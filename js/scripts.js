e3c485ae7407cabbbcd837191e01031710d0d5cd
eluts <eluts15@gmail.com>
2017-05-08 23:38:30 -0700

    and html features and add triangle feature in javascript.

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+//Logic
+
+
+
+
+
+
+       //\\
+      //  \\
+     //    \\
+    //      \\
+   //        \\
+  //          \\
+ //            \\
+//User Interface\\
+
+$(function() { //iife
+  $("#submit-form").submit(event) {
+    
+    event.preventDefault();
+
+
+    var languagePref = $("#language-pref").val(); //Should store as a string i think.
+    var choice = $("input:radio[name=choice]:checked").val();
+    var trueOrFalse = $("input:checkbox[]]")
+
+ 
+    if (languagePref === "golang" || languagePref === "csharp") {
+            console.log("Try something backend related.");
+                        $("#recommended").show();
+                        $(".recommendation").append("C#");
+
+    }
+    else if (languagePref === "design") {
+            console.log("Consider something design oriented.");
+                        $("#reccomended").show();
+                        $(".recommendation").append("DESIGN");
+    }
+    else {
+                        $("#recommended").show();
+                        $(".recommendation").append("PHP");          
+    }
+    
+  };
+});
