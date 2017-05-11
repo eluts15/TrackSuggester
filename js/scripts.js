f1c865e28ffd8cc59ba562729d2d90bf94baaed4
eluts <eluts15@gmail.com>
2017-05-11 09:00:47 -0700

    hide unwanted elements, now works as intended.

--- js/scripts.js
+++ js/scripts.js
@@ -13,20 +13,26 @@ $(function() { //iife
 
     var languagePref = $("#language-pref").val(); //just compare strings
     var drinkPref = parseInt($("#drank").val()); //parse this one as int for science.
-    var trueOrFalse = parseInt($("#ux").val())
+    //var trueOrFalse = parseInt($("#ux").val()) 
 
 
     if (languagePref === "GOLANG" || languagePref === "C#" && !"DESIGN") { //check for a matching string. 
-        $("#recommendation").hide();
+        $("#recommendation2").hide();
+        $("#recommendation3").hide();
         $("#recommendation").show();
-
     }
-
     else if (drinkPref > 3 && drinkPref < 6) { //what you drink determines what kind of coding interests.haha
+      $("#recommendation").hide();
+      $("#recommendation3").hide();
       $("#recommendation2").show();
-        
     }
-    else { $("#recommendation3").show(); }
+    else {
+       $("#recommendation3").show();
+       $("#recommendation").hide() 
+       $("#recommendation2") .hide();
+    }
+
+	   
 
   });
 });
